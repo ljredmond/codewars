407af30dc7ce6b6ae972c7e6cb182c47bfd3a6d0
ljredmond <redmondalouis@gmail.com>
2018-09-05 18:41:00 -0500

    updated Multiply documentation

--- java/src/main/java/com/ljredmond/codewars/solutions/kyu8/Multiply.java
+++ java/src/main/java/com/ljredmond/codewars/solutions/kyu8/Multiply.java
@@ -2,6 +2,15 @@ package com.ljredmond.codewars.solutions.kyu8;
 
 public class Multiply {
 
+  /**
+   * https://www.codewars.com/kata/50654ddff44f800200000004
+   *
+   * Multiplies two numbers
+   *
+   * @param a the first number
+   * @param b the second number
+   * @return a * b
+   */
   public static Double multiply(Double a, Double b) {
 
     return a * b;
