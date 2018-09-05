232a0b6fb532ef8ff75838cee636926830a6c762
ljredmond <redmondalouis@gmail.com>
2018-09-05 18:40:05 -0500

    updated BinaryArrayToNumber documentation

--- java/src/main/java/com/ljredmond/codewars/solutions/kyu7/BinaryArrayToNumber.java
+++ java/src/main/java/com/ljredmond/codewars/solutions/kyu7/BinaryArrayToNumber.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 public class BinaryArrayToNumber {
 
+  /**
+   * https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+   *
+   * Converts a list of one's and zero's to the integer value
+   *
+   * ex.) [0, 0, 0, 1] -> 1
+   * ex.) [1, 1, 0, 1] -> 13
+   *
+   * @param binary the list of bits
+   * @return the equivalent integer value
+   */
   public static int convertBinaryArrayToInt(List<Integer> binary) {
 
     int factor = 1;
